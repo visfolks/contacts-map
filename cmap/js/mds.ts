07784985fb38600c7a8908dff89890bd6a83742b
mukundraj <mukundraj2@gmail.com>
2021-07-31 20:18:00 -0400

    Initial files

---
+++ cmap/js/mds.ts
@@ -0,0 +1,14 @@
+
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "First", lastName: "Last" };
+
+document.body.textContent = greeter(user);
